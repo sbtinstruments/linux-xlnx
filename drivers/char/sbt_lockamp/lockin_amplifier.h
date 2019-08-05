5f61adb65ebf08c8559724ce6487b30ddddd2e6b
Frederik Peter Aalund <fpa@sbtinstruments.com>
2019-08-05 17:08:02 +0200

    char: sbt_lockamp: Rework driver
     - Add power management support (including runtime pm).
     - Remove 'lockamp-config2' and 'lockamp-fir' iomem resources.
       All aspects of the hardware is now controlled through the
       'lockamp-config' iomem address range.
     - Various internal cleanup and refactoring.

--- drivers/char/sbt_lockamp/lockin_amplifier.h
+++ drivers/char/sbt_lockamp/lockin_amplifier.h
@@ -1,20 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * SBT Instruments Lock-in Amplifier
+ *
+ * Copyright (c) 2019, Frederik Peter Aalund <fpa@sbtinstruments.com>
+ */
 #ifndef LOCKAMP_LOCKAMP_H
 #define LOCKAMP_LOCKAMP_H
 #include <asm/atomic.h>
 #include <linux/cdev.h>
+#include <linux/pm_runtime.h>
 #include <linux/sysfs.h>
+
+struct sample;
 
 /* Class name as it appears in /sys/class  */
 #define LOCKAMP_CLASS_NAME  "lockin_amplifier"
-#define LOCKAMP_MSG LOCKAMP_CLASS_NAME ": "
 
 #define LOCKAMP_ADC_SAMPLES_SIZE_S32 16384
-#define LOCKAMP_ADC_SAMPLES_SIZE (LOCKAMP_ADC_SAMPLES_SIZE_S32 * sizeof(s32))
-/* The following define omits various memory barrier instructions
- *
- * E.g., Removes the "dsb sy" instruction in lockamp_fifo_pop on ARM.
- * */
-#define LOCKAMP_FAVOR_SPEED_OVER_SAFETY
+#define LOCKAMP_ADC_SAMPLES_SIZE     (LOCKAMP_ADC_SAMPLES_SIZE_S32 * sizeof(s32))
+#define LOCKAMP_FIFO_CAPACITY        131072
+#define LOCKAMP_FIFO_CAPACITY_N      (LOCKAMP_FIFO_CAPACITY / sizeof(struct sample))
+#define LOCKAMP_FIR_FILTER_COUNT     8
+#define LOCKAMP_FIR_COEF_LEN         512
+#define LOCKAMP_SITES_PER_SAMPLE     2
+#define LOCKAMP_ENTRIES_PER_SITE     4
+#define LOCKAMP_ENTRIES_PER_SAMPLE   (LOCKAMP_ENTRIES_PER_SITE * LOCKAMP_SITES_PER_SAMPLE)
+#define LOCKAMP_SIGNAL_BUF_CAPACITY  4194304 /* 4 MiB */
 
 struct circ_sample_buf {
 	struct sample *buf;
@@ -33,9 +44,8 @@ struct lockamp {
 	struct cdev cdev;
 	struct device *dev;
 	dev_t chrdev_no;
+	struct iio_channel *adc_site0, *adc_site1, *dac_site0, *dac_site1;
 	u32 __iomem *control;
-	u32 __iomem *control2;
-	u32 __iomem *fir;
 
 	struct circ_sample_buf signal_buf;
 	struct mutex signal_buf_m;
@@ -48,7 +58,6 @@ struct lockamp {
 	u64 last_start_time_ns;
 };
 
-#define LOCKAMP_ENTRIES_PER_SITE 4
 struct site_sample {
 	s32 hf_re;
 	s32 hf_im;
@@ -57,8 +66,6 @@ struct site_sample {
 } __attribute__((packed));
 _Static_assert (LOCKAMP_ENTRIES_PER_SITE * sizeof(s32) == sizeof(struct site_sample), "struct 'site_sample' is not packed on this platform");
 
-#define LOCKAMP_SITES_PER_SAMPLE 2
-#define LOCKAMP_ENTRIES_PER_SAMPLE (LOCKAMP_ENTRIES_PER_SITE * LOCKAMP_SITES_PER_SAMPLE)
 struct sample {
 	struct site_sample sites[LOCKAMP_SITES_PER_SAMPLE];
 } __attribute__((packed));
@@ -70,10 +77,9 @@ extern u64 lockamp_fifo_read_delay;
 extern int lockamp_debug1;
 extern int lockamp_debug2;
 extern int lockamp_debug3;
-extern const size_t lockamp_fifo_capacity_n;
-#define LOCKAMP_FIR_FILTER_COUNT 8
-extern const s32 lockamp_fir_coefs[LOCKAMP_FIR_FILTER_COUNT][512];
+extern const s32 lockamp_fir_coefs[LOCKAMP_FIR_FILTER_COUNT][LOCKAMP_FIR_COEF_LEN];
 extern const struct attribute_group *lockamp_attr_groups[2];
 extern struct file_operations lockamp_fops;
+extern struct dev_pm_ops lockamp_pm_ops;
 
 #endif /* LOCKAMP_LOCKAMP_H */
